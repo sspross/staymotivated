40b9fde6f8f5fecbc9ec99bec59deb953f0e4e48
Andreas Grimm <grimm@netdot.ch>
2014-06-03 00:03:48 +0200

    Removed TaskForm and integrated it in Task. Task has now state editable

--- static/js/app.js
+++ static/js/app.js
@@ -26,8 +26,12 @@ var StayMotivatedTable = React.createClass({
   },
   addNewTask: function() {
     if (this.state.newTask === null) {
-      this.setState({newTask: <TaskForm onTaskCancel={this.cancelNewTask} onTaskSave={this.saveNewTask} />});
+      this.setState({newTask: <Task editable="true" onTaskCancel={this.cancelNewTask} onTaskSave={this.saveNewTask} />});
     }
+  },
+  deleteTask: function(taskKey) {
+    this.state.week.tasks.splice(taskKey, 1);
+    this.forceUpdate();
   },
   cancelNewTask: function() {
     this.setState({newTask: null});
@@ -38,9 +42,9 @@ var StayMotivatedTable = React.createClass({
     this.state.week.tasks.push({name: name, goal: goal, achievedDays: [], feasibleDays: feasibleDays});
     this.cancelNewTask();
   },
-  handleDayClick: function(key, day) {
+  handleDayClick: function(taskKey, day) {
     // TODO replace this part with server side update, not using setState() and forceUpdate() is evil
-    var task = this.state.week.tasks[key];
+    var task = this.state.week.tasks[taskKey];
     var dayIndex = jQuery.inArray(day, task.achievedDays);
     if (dayIndex >= 0) {
       task.achievedDays.splice(dayIndex, 1);
@@ -55,7 +59,7 @@ var StayMotivatedTable = React.createClass({
       var number = this.state.week.number;
       var year = this.state.week.year;
       this.state.week.tasks.forEach(function(task, i) {
-          tasks.push(<Task key={i} name={task.name} goal={task.goal} achievedDays={task.achievedDays} feasibleDays={task.feasibleDays} onDayClick={this.handleDayClick} />);
+          tasks.push(<Task key={i} name={task.name} goal={task.goal} achievedDays={task.achievedDays} feasibleDays={task.feasibleDays} onDayClick={this.handleDayClick} onDelete={this.deleteTask} />);
       }.bind(this));
     }
     return (
@@ -111,30 +115,71 @@ var StayMotivatedTable = React.createClass({
 
 
 var Task = React.createClass({
-  render: function() {
-    var numAchieved = Object.keys(this.props.achievedDays).length;
-    var isCompleted = numAchieved >= this.props.goal ? true : false
-    var isOverdone = isCompleted && numAchieved > this.props.goal ? true : false;
-    var days = [];
+  getInitialState: function(){
+    return {editable: this.props.editable};
+  },
+  saveTask: function() {
+    var name = this.refs.taskName.getDOMNode().value;
+    var goal = this.refs.goal.getDOMNode().value
+    var feasibleDays = [];
     WEEKDAYS.forEach(function(day) {
-      var feasible = jQuery.inArray(day, this.props.feasibleDays) >= 0 ? true : false;
-      var achieved = jQuery.inArray(day, this.props.achievedDays) >= 0 ? true : false;
-      days.push(<Day taskKey={this.props.key} day={day} feasible={feasible} achieved={achieved} onDayClick={this.props.onDayClick} />);
+      if (this.refs[day].getDOMNode().checked) {
+        feasibleDays.push(day);
+      }
     }.bind(this));
-    return (
-      <tr className={isCompleted ? "success" : ""}>
-        <td className="state-col"><span className={isCompleted ? "glyphicon glyphicon-star" : "glyphicon glyphicon-star-empty"}></span></td>
-        <td>
-          {this.props.name}
-          <div className="pull-right btn-group hover-btn-group">
-            <a className="btn btn-default btn-xs" href="#" role="button"><span className="glyphicon glyphicon-pencil"></span></a>
-            <a className="btn btn-default btn-xs" href="#" role="button"><span className="glyphicon glyphicon-trash"></span></a>
-          </div>
-        </td>
-        {days}
-        <td>{numAchieved} / {this.props.goal} <span className={isOverdone ? "glyphicon glyphicon-heart" : ""}></span></td>
-      </tr>
-    );
+    this.props.onTaskSave(name, goal, feasibleDays);
+    this.setState({editable: false})
+  },
+  makeEditable: function() {
+    this.setState({editable: true}) 
+  },
+  render: function() {
+    if (this.state.editable) {
+      var days = [];
+      WEEKDAYS.forEach(function(day) {
+        days.push(<td className="day-col"><input type="checkbox" ref={day}></input></td>);
+      }.bind(this));
+      return (
+        <tr>
+          <td></td>
+          <td className="form-inline">
+            <input type="text" className="form-control input-sm" ref="taskName" placeholder="Task name..." value={this.props.name}></input>
+            <div className="pull-right btn-group">
+              <button type="submit" className="btn btn-default btn-sm" onClick={this.saveTask}><span className="glyphicon glyphicon-floppy-disk"></span></button>
+              <a className="btn btn-default btn-sm" onClick={this.props.onTaskCancel}><span className="glyphicon glyphicon-ban-circle"></span></a>
+            </div>
+          </td>
+          {days}
+          <td>
+            <input type="number" className="form-control input-sm" ref="goal" placeholder="Goal...">{this.props.goal}</input>
+          </td>
+        </tr>
+      );
+    } else {
+      var numAchieved = Object.keys(this.props.achievedDays).length;
+      var isCompleted = numAchieved >= this.props.goal ? true : false
+      var isOverdone = isCompleted && numAchieved > this.props.goal ? true : false;
+      var days = [];
+      WEEKDAYS.forEach(function(day) {
+        var feasible = jQuery.inArray(day, this.props.feasibleDays) >= 0 ? true : false;
+        var achieved = jQuery.inArray(day, this.props.achievedDays) >= 0 ? true : false;
+        days.push(<Day taskKey={this.props.key} day={day} feasible={feasible} achieved={achieved} onDayClick={this.props.onDayClick} />);
+      }.bind(this));
+      return (
+        <tr className={isCompleted ? "success" : ""}>
+          <td className="state-col"><span className={isCompleted ? "glyphicon glyphicon-star" : "glyphicon glyphicon-star-empty"}></span></td>
+          <td>
+            {this.props.name}
+            <div className="pull-right btn-group hover-btn-group">
+              <a className="btn btn-default btn-xs" href="#" role="button" onClick={this.makeEditable}><span className="glyphicon glyphicon-pencil"></span></a>
+              <a className="btn btn-default btn-xs" href="#" role="button" onClick={this.props.onDelete.bind(null, this.props.key)}><span className="glyphicon glyphicon-trash"></span></a>
+            </div>
+          </td>
+          {days}
+          <td>{numAchieved} / {this.props.goal} <span className={isOverdone ? "glyphicon glyphicon-heart" : ""}></span></td>
+        </tr>
+      );
+    }
   }
 });
 
@@ -147,43 +192,6 @@ var Day = React.createClass({
       <td className={tdClass} onClick={this.props.onDayClick.bind(null, this.props.taskKey, this.props.day)}>
         <span className={spanClass}></span>
       </td>
-    );
-  }
-});
-
-
-var TaskForm = React.createClass({
-  saveTask: function() {
-    var name = this.refs.taskName.getDOMNode().value;
-    var goal = this.refs.goal.getDOMNode().value
-    var feasibleDays = [];
-    WEEKDAYS.forEach(function(day) {
-      if (this.refs[day].getDOMNode().checked) {
-        feasibleDays.push(day);
-      }
-    }.bind(this));
-    this.props.onTaskSave(name, goal, feasibleDays);
-  },
-  render: function() {
-    var days = [];
-    WEEKDAYS.forEach(function(day) {
-      days.push(<td className="day-col"><input type="checkbox" ref={day}></input></td>);
-    }.bind(this));
-    return (
-      <tr>
-        <td></td>
-        <td className="form-inline">
-          <input type="text" className="form-control input-sm" ref="taskName" placeholder="Task name...">{this.props.name}</input>
-          <div className="pull-right btn-group">
-            <button type="submit" className="btn btn-default btn-sm" onClick={this.saveTask}><span className="glyphicon glyphicon-floppy-disk"></span></button>
-            <a className="btn btn-default btn-sm" onClick={this.props.onTaskCancel}><span className="glyphicon glyphicon-ban-circle"></span></a>
-          </div>
-        </td>
-        {days}
-        <td>
-          <input type="number" className="form-control input-sm" ref="goal" placeholder="Goal...">{this.props.goal}</input>
-        </td>
-      </tr>
     );
   }
 });
